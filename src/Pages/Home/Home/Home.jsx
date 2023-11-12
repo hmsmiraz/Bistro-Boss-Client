8c3075c10dfbae30491432793e2586029dd356f3
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-13 01:32:36 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <p className="font-bold text-center text-2xl text-stone-600">This is home</p>
+        </div>
+    );
+};
+
+export default Home;
