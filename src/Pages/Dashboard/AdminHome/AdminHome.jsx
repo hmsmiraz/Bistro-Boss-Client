0b66bf0929b1170f1d460a50df2af4772a46a892
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-23 12:39:31 +0600

    added order stats

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -7,13 +7,16 @@ const AdminHome = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: stats } = useQuery({
+  const { data: stats = {} } = useQuery({
     queryKey: ["admin-stats"],
     queryFn: async () => {
       const res = await axiosSecure.get("/admin-stats");
       return res.data;
     }
   });
+  const revenue = stats?.revenue;
+  const newRevenue = revenue.toFixed(2);
+//   console.log(newRevenue)
   return (
     <div>
       <div className="text-center font-bold text-emerald-600 text-3xl">
@@ -22,34 +25,34 @@ const AdminHome = () => {
           {user?.displayName ? user.displayName : "Back"}
         </h2>
       </div>
-      <div className="stats shadow flex justify-end items-center">
+      <div className="stats shadow flex justify-end items-center my-4">
         <div className="stat">
           <div className="stat-figure text-secondary text-4xl">
             <FaDollarSign />
           </div>
           <div className="stat-title">Revenue</div>
-          <div className="stat-value">{stats.revenue}</div>
+          <div className="stat-value">{newRevenue}</div>
         </div>
         <div className="stat">
           <div className="stat-figure text-secondary text-3xl">
             <FaUsers />
           </div>
           <div className="stat-title">Users</div>
-          <div className="stat-value">{stats.users}</div>
+          <div className="stat-value">{stats?.users}</div>
         </div>
         <div className="stat">
           <div className="stat-figure text-secondary text-3xl">
             <FaUtensilSpoon />
           </div>
           <div className="stat-title">Products</div>
-          <div className="stat-value">{stats.menuItems}</div>
+          <div className="stat-value">{stats?.menuItems}</div>
         </div>
         <div className="stat">
           <div className="stat-figure text-secondary text-3xl">
             <FaTruck />
           </div>
           <div className="stat-title">Orders</div>
-          <div className="stat-value">{stats.orders}</div>
+          <div className="stat-value">{stats?.orders}</div>
         </div>
       </div>
     </div>
